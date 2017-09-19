a36a35d1ef9ca1ff499e6bdbbd926a37dc0da868
Nick Batson <nickbatson174@gmail.com>
2017-09-19 22:40:32 +1000

    Hiding Journals if users arent logged in

--- routes/index.js
+++ routes/index.js
@@ -10,15 +10,9 @@ router.use(bodyParser.urlencoded({ extended: true}));
 
 var url = "mongodb://localhost:27017/users";
 
-/* GET home page. */
-router.post('/', function (req, res, next) {
-    console.log(req.body)
-});
 
-//Show the login page
-router.get('/signin', function (req, res, next) {
-    res.render('login.pug', { user: req.user });
-});
+
+
 
 //Show the registration page
 router.get('/register', function (req, res, next) {
@@ -47,58 +41,6 @@ router.post('/register', function (req, res) {
     });
 });
 
-
-/*
-router.post('/register', function (req, res) {
-    Account.register(new Account)
-
-    console.log("registration recieved");
-
-
-    var fullName = req.body.name;
-    var username = req.body.username;
-    var password = req.body.password;
-    var email = req.body.email;
-    var colour = req.body.usercolor;
-
-    var upload = true
-    if (!fullName || !username || !password || !email || !colour)
-        upload = false
-
-    if (upload == true)
-        MongoClient.connect(url, function (err, db) {
-
-
-            if (err) throw err;
-            var newUser = {
-                full_name: fullName,
-                user_name: username,
-                password: password,
-                email_address: email,
-                admin: false,
-                colour_choice: colour
-            };
-
-
-            db.collection("users").insertOne(newUser, function (err, res) {
-                if (err) throw err;
-                console.log("1 document inserted");
-                db.close();
-         });
-        });
-
-
-    if (upload == true) {
-        res.redirect('/journals');
-    } else {
-        //var errormsg = "Please complete all required fields"
-        res.render('register.pug', { errormsg: 'Please complete all required fields' })
-    }
-
-});
-*/
-
-
 //Show the journals page
 router.get('/journals', function (req, res, next) {
     res.render('journals.pug', {user: req.user});
@@ -108,21 +50,34 @@ router.get('/searchJournals', function (req, res, next) {
     res.render('searchJournals.pug')
 });
 
+/* Logs the user in */
 router.post('/signin', passport.authenticate('local'), function (req, res) {
     //console.log(user.username)
     res.redirect('/journals');
 });
 
+//Show the login page
+router.get('/signin', function (req, res, next) {
+    res.render('login.pug', { user: req.user });
+});
+
+
+/*NOT COMPLETE*/
 router.get('/logout', function (req, res) {
     req.logout();
     res.redirect('/');
 });
 
 
-/* GET home page. */
+/* Provides the index page */
 router.get('/', function (req, res, next) {
     res.render('index.pug', { user: req.user });
     //res.render('index.pug', { title: 'World!', example: ['hello', 'guys', 'this', 'is', 'an', 'example'] });
+});
+
+/* Gets data from the index page. */
+router.post('/', function (req, res, next) {
+    console.log(req.body)
 });
 
 module.exports = router;
