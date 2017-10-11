686f5f9bbdd0a2acbdc573adb6b3ad333071f42f
Nick Batson <nickbatson174@gmail.com>
2017-10-11 22:36:04 +1100

    Tests and modules

--- tests/db_test.js
+++ tests/db_test.js
@@ -2,16 +2,121 @@ var chai = require('chai');
 var expect = chai.expect; // we are using the "expect" style of Chai
 var routesConnect = require('./../public/javascripts/unitTest.js')
 
-describe('databaseTest', function () {
+describe('Database tests', function () {
 	it('Check that the database is connecting to the app', function () {
 		expect(routesConnect.dbRunning()).to.equal(2);
 	});
 
 	it('Check that the database can find a user', function () {
-		//console.log('***');
-		console.log(routesConnect.findUser());
-		//console.log('***');
-		expect(routesConnect.findUser()).to.not.equal(undefined);
-	});
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+    it('Check that we can add a user', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+    it('Check the user has a name', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+    it('Check the user has a unique id', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+    it('Check that we can remove a user', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+});
+
+describe('Journal tests', function () {
+
+    it('Add a temporary user', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+    it('Check that we can add a journal', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+    it('Check that we can get a journal we have added', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+    it('Check that we can delete a journal', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+    it('Removing temporary user', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+});
+
+describe('Entry tests', function () {
+
+    it('Add a temporary user', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+    it('Add a temporary journal', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+    it('Check that we can add an entry', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+    it('Check the entry has a name', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+    it('Check the entry has text', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+    it('Check that we can modify the text', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+    it('Check that the text has been modified', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+    it('Delete entry', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+    it('Check entry does not exist', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+    it('Remove temporary journal', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+    it('Remove temporary user', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+});
+
+describe('User tests', function () {
+
+    it('Add a temporary user', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+    it('Change the users color', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+    it('Check the background color', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
+
+    it('Remove temporary user', function () {
+        expect(routesConnect.dbRunning()).to.equal(2);
+    });
 
 });
